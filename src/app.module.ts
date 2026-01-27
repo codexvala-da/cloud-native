00fd1c76fdd7cbd9a458f818dff4bb77728d9df5
codexvala-da <vala.da@northeastern.edu>
2026-01-26 21:38:03 -0500

    user module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { UsersModule } from './user/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'local-postgres',
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
